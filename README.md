2280872274dc660edae5369c3db14846a5d876c9
unadlib <unadlib@gmail.com>
2024-05-19 04:05:19 +0800

    chore(init): init project

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # zustand-mutative
 A Mutative middleware for Zustand enhances the efficiency of immutable state updates.
+
+## Installation
+```bash
+npm install zustand-mutative
+```
+
+## Usage
+```javascript
+import { create } from 'zustand'
+import { mutative } from 'zustand-mutative'
+
+type State = {
+  count: number
+}
+
+type Actions = {
+  increment: (qty: number) => void
+  decrement: (qty: number) => void
+}
+
+export const useCountStore = create<State & Actions>()(
+  mutative((set) => ({
+    count: 0,
+    increment: (qty: number) =>
+      set((state) => {
+        state.count += qty
+      }),
+    decrement: (qty: number) =>
+      set((state) => {
+        state.count -= qty
+      }),
+  })),
+)
+```
---
+++ rollup.config.ts
@@ -0,0 +1,48 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+const input = './dist/index.js';
+
+export default {
+  input,
+  output: [
+    {
+      format: 'cjs',
+      exports: 'auto',
+      file: 'dist/index.cjs.js',
+      sourcemap: true,
+    },
+    {
+      format: 'es',
+      file: 'dist/index.esm.js',
+      sourcemap: true,
+    },
+    {
+      format: 'umd',
+      name: pkg.name
+        .split('-')
+        .map(([s, ...rest]) => [s.toUpperCase(), ...rest].join(''))
+        .join(''),
+      file: pkg.unpkg,
+      sourcemap: true,
+      globals: {
+        mutative: 'Mutative',
+        react: 'React',
+      },
+      exports: 'named',
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    replace({
+      __DEV__: 'false',
+      preventAssignment: true,
+    }),
+    terser(),
+  ],
+  external: ['mutative', 'react'],
+};
