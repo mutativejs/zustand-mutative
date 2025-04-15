c2a8166a0d34f63d299d0f3fde8f543b552d8610
unadlib <unadlib@gmail.com>
2025-04-16 01:23:49 +0800

    fix(type): resolve NodeNext import issues

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,9 +2,14 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import { glob } from 'glob';
+import fs from 'fs-extra';
+import path from 'path';
+
 import pkg from './package.json';
 
-const input = './dist/index.js';
+const input = './src/index.ts';
 
 export default {
   input,
@@ -39,11 +44,35 @@ export default {
   plugins: [
     resolve(),
     commonjs(),
+    typescript({
+      declaration: true,
+      declarationDir: 'dist',
+    }),
     replace({
       __DEV__: 'false',
       preventAssignment: true,
     }),
     terser(),
+    {
+      name: 'copy-dts-to-dmts',
+      writeBundle(options, bundle) {
+        console.log('Copying .d.ts to .d.mts...');
+
+        const files = glob.sync(`dist/**/*.d.ts`, { absolute: true });
+        files.forEach((filePath) => {
+          const mtsPath = filePath.replace(/\.d\.ts$/, '.d.mts');
+          try {
+            fs.copySync(filePath, mtsPath);
+            console.log(
+              `  Copied ${path.basename(filePath)} to ${path.basename(mtsPath)}`
+            );
+          } catch (err) {
+            console.error(`  Error copying ${filePath} to ${mtsPath}:`, err);
+          }
+        });
+        console.log('.d.mts generation complete.');
+      },
+    },
   ],
   external: ['mutative', 'zustand', 'react'],
 };
